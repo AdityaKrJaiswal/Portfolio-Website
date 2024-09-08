f61cc944f140c44269e31f12aa31b8ec51ecb03f
Aditya Jaiswal <adityakumarjaiswal029@gmail.com>
2024-09-08 22:55:18 +0530

    hello

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,20 +21,20 @@ const Contact = () => {
       toast.success("Your message has been sent successfully");
     } catch (e) {
       console.log("Error in sending data to server", e);
-      toast.error("Error sending data to server")
+      toast.error("Error sending data to server");
     }
   };
 
   return (
     <div
       name="Contact"
-      className="container max-w-screen-xl mx-auto px-4 md:px-20 mt-20 pt-5 md:mt-10"
+      className="container max-w-screen-xl mx-auto px-4 md:px-20 mt-20 pt-20 md:mt-10"
     >
       <h1 className="w-full text-2xl mb-5 font-bold text-center">Contact</h1>
       <div className="flex flex-col justify-center items-center">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="bg-slate-500 w-96 px-8 py-6 rounded-xl "
+          className="bg-slate-500 w-full max-w-md md:max-w-lg px-8 py-6 rounded-xl"
         >
           <h1 className="text-lg font-bold">Send a Note</h1>
           <div className="flex flex-col mt-4">
@@ -49,7 +49,9 @@ const Contact = () => {
               className="w-full mt-2 p-2 rounded-lg"
               placeholder="Enter Your Name"
             />
-            {errors.name && <span className="text-red-500">This field is required</span>}
+            {errors.name && (
+              <span className="text-red-500">This field is required</span>
+            )}
           </div>
           <div className="flex flex-col mt-4">
             <label className="block text-gray-700" htmlFor="email">
@@ -63,7 +65,9 @@ const Contact = () => {
               className="w-full mt-2 p-2 rounded-lg"
               placeholder="Enter Your Email-Address"
             />
-            {errors.email && <span className="text-red-500">This field is required</span>}
+            {errors.email && (
+              <span className="text-red-500">This field is required</span>
+            )}
           </div>
           <div className="flex flex-col mt-4">
             <label className="block text-gray-700" htmlFor="message">
@@ -73,14 +77,16 @@ const Contact = () => {
               {...register("message", { required: true })}
               id="message"
               name="message"
-              className="w-full h-20 mt-2 px-2 rounded-lg"
+              className="w-full mt-2 p-2 rounded-lg min-h-[80px] h-auto"
               placeholder="Write your message here"
             />
-            {errors.message && <span className="text-red-500">This field is required</span>}
+            {errors.message && (
+              <span className="text-red-500">This field is required</span>
+            )}
           </div>
           <button
             type="submit"
-            className="text-lg font-bold mt-3 px-3 bg-zinc-500 rounded-xl py-2 hover:bg-slate-300 hover:text-gray-900 "
+            className="text-lg font-bold mt-3 px-3 bg-zinc-500 rounded-xl py-2 hover:bg-slate-300 hover:text-gray-900"
           >
             Send
           </button>
